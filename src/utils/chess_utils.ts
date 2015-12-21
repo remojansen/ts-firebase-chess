07e531714814661eb067e1a8dcb0bb79d1b3fb0f
remojansen <remo.jansen@wolksoftware.com>
2015-12-21 04:40:24 +0000

    added movements

--- src/utils/chess_utils.ts
+++ src/utils/chess_utils.ts
@@ -186,16 +186,99 @@ export function flagAvailableCells(board, figures) {
 }
 
 function cellContainsFigure(position : IPosition, board : ICell[][]) {
-  if(position.y < 0 || position.y > 7 || position.x < 0 || position.y > 7) return false;
+  if(position.x < 0 || position.x > 7 || position.y < 0 || position.y > 7) return false;
   var cell = board[position.y][position.x];
   return (cell.figure !== null && cell.figure.alive !== false);
 }
 
 function cellContainsEnemyFigure(color: string, position : IPosition, board : ICell[][]) {
-  if(position.y < 0 || position.y > 7 || position.x < 0 || position.y > 7) return false;
+  if(position.x < 0 || position.x > 7 || position.y < 0 || position.y > 7) return false;
   if (cellContainsFigure(position, board) === false) return false;
   var cell = board[position.y][position.x];
   return (cell.figure !== null && cell.figure.color !== color && cell.figure.alive !== false);
+}
+
+function getMove(figure, x, y, board, moves) {
+  let p = { x : x, y : y};
+  if(p.x < 0 || p.x > 7 || p.y < 0 || p.y > 7) return null;
+  if(cellContainsEnemyFigure(figure.color, p, board) === true) { moves.push(p);  return null; }
+  if(cellContainsFigure(p, board) === true) return null;
+  return p;
+}
+
+function diagonalMoves(figure, board) {
+
+  var moves = [];
+  var y = 0;
+  var x = 0;
+
+  // bishop can move diagonally down-left until hit a figure
+  y = figure.position.y;
+  for(let x = (figure.position.x - 1); x > -1; x--) {
+    y = y + 1;
+    var ml = getMove(figure, x, y, board, moves);
+    if(ml == null) break;
+    moves.push(ml);
+  }
+
+  // bishop can move diagonally down-right until hit a figure
+  y = figure.position.y;
+  for(let x = (figure.position.x + 1); x < 8; x++) {
+    y = y + 1;
+    var mr = getMove(figure, x, y, board, moves);
+    if(mr == null) break;
+    moves.push(mr);
+  }
+
+  // bishop can move diagonally up-left until hit a figure
+  y = figure.position.y;
+  for(let x = (figure.position.x - 1); x > -1; x--) {
+    y = y - 1;
+    var ml = getMove(figure, x, y, board, moves);
+    if(ml == null) break;
+    moves.push(ml);
+  }
+
+  // bishop can move diagonally up-right until hit a figure
+  y = figure.position.y;
+  for(let x = (figure.position.x + 1); x < 8; x++) {
+    y = y - 1;
+    var mr = getMove(figure, x, y, board, moves);
+    if(mr == null) break;
+    moves.push(mr);
+  }
+
+  return moves;
+}
+
+function verticalAndHorizontalMoves(figure, board) {
+
+  var moves = [];
+  // tower can move left until hit a figure
+  for(let x = (figure.position.x - 1); x > -1; x--) {
+    var ml = getMove(figure, x, figure.position.y, board, moves);
+    if(ml == null) break;
+    moves.push(ml);
+  }
+  // tower can move right until hit a figure
+  for(let x = (figure.position.x + 1); x < 8; x++) {
+    var mr = getMove(figure, x, figure.position.y, board, moves);
+    if(mr == null) break;
+    moves.push(mr);
+  }
+  // tower can move up until hit a figure
+  for(let y = (figure.position.y - 1); y > -1; y--) {
+    var mu = getMove(figure, figure.position.x, y, board, moves);
+    if(mu == null) break;
+    moves.push(mu);
+  }
+  // tower can move down until hit a figure
+  for(let y = (figure.position.y + 1); y < 8; y++) {
+    var md = getMove(figure, figure.position.x, y, board, moves);
+    if(md == null) break;
+    moves.push(md);
+  }
+  return moves;
 }
 
 export function getFigureAvailableMoves(figure : IFigure, board : ICell[][]) : IPosition[] {
@@ -209,20 +292,57 @@ export function getFigureAvailableMoves(figure : IFigure, board : ICell[][]) : I
 
   switch(figure.type) {
     case CONST.FIGURE_KING:
+      // king can move one cell in all directions
+      let kingPositions = [
+        { x : figure.position.x, y : figure.position.y + 1 },
+        { x : figure.position.x, y : figure.position.y - 1 },
+        { x : figure.position.x + 1, y : figure.position.y },
+        { x : figure.position.x - 1, y : figure.position.y },
+        { x : figure.position.x - 1, y : figure.position.y - 1 },
+        { x : figure.position.x - 1, y : figure.position.y + 1 },
+        { x : figure.position.x + 1, y : figure.position.y - 1 },
+        { x : figure.position.x + 1, y : figure.position.y + 1 }
+      ];
+      kingPositions.forEach((p) => {
+        var containsFigure = cellContainsFigure(p, board);
+        var containsEnemy = cellContainsEnemyFigure(figure.color, p, board);
+        if(containsFigure === false || containsEnemy === true) moves.push(p);
+      });
       break;
     case CONST.FIGURE_QUEEN:
+      moves = verticalAndHorizontalMoves(figure, board).concat(diagonalMoves(figure, board));
       break;
     case CONST.FIGURE_BISHOP:
-      break;
-    case CONST.FIGURE_KNIGHT:
+      moves = diagonalMoves(figure, board);
       break;
     case CONST.FIGURE_TOWER:
+      moves = verticalAndHorizontalMoves(figure, board);
+      break;
+    case CONST.FIGURE_KNIGHT:
+      // knight can move in L-like paths (two foward and one leaf/right cell)
+      let knightPositions = [
+        { x : figure.position.x - 1, y : figure.position.y - 2 },
+        { x : figure.position.x + 1, y : figure.position.y - 2 },
+        { x : figure.position.x + 2, y : figure.position.y + 1 },
+        { x : figure.position.x + 2, y : figure.position.y - 1 },
+        { x : figure.position.x - 2, y : figure.position.y + 1 },
+        { x : figure.position.x - 2, y : figure.position.y - 1 },
+        { x : figure.position.x - 1, y : figure.position.y + 2 },
+        { x : figure.position.x + 1, y : figure.position.y + 2 }
+      ];
+      knightPositions.forEach((p) => {
+        var containsFigure = cellContainsFigure(p, board);
+        var containsEnemy = cellContainsEnemyFigure(figure.color, p, board);
+        if(containsFigure === false || containsEnemy === true) moves.push(p);
+      });
       break;
     case CONST.FIGURE_PAWN:
       // pawn can move two cells foward if first move
       if(figure.hasMoved === false) {
-        moves.push({ x : figure.position.x, y : figure.position.y + (yMovement * 1) });
-        moves.push({ x : figure.position.x, y : figure.position.y + (yMovement * 2) });
+        let p1 = { x : figure.position.x, y : figure.position.y + (yMovement * 1) };
+        let p2 = { x : figure.position.x, y : figure.position.y + (yMovement * 2) };
+        if(cellContainsFigure(p1, board) === false) moves.push(p1);
+        if(cellContainsFigure(p2, board) === false) moves.push(p2);
       }
       // pawn can move only one step foward if not already in use
       else {
